80c6624727f7c6a094b308843cc6d7ea1a2390e6
Ihago estudos <ihago.oliveira@gmail.com>
2025-03-26 14:44:55 -0300

    feat: Add pagina de novo cadastrp de categorias

--- app/controllers/administrate/categories_controller.rb
+++ app/controllers/administrate/categories_controller.rb
@@ -5,7 +5,7 @@ module Administrate
     # before_action :set_article, only: [:show, :edit, :update, :destroy, :destroy_cover_image]
     # before_action :set_categories, only: [:new, :edit, :show]
 
-    # GET /articles or /articles.json
+    # GET /categories or /categories.json
     def index
       @categories = Category.all
     end
@@ -14,9 +14,9 @@ module Administrate
     def show
     end
 
-    # GET /articles/new
+    # GET /categories/new
     def new
-      # @article = Article.new
+      @category = Category.new
     end
 
     # GET /articles/1/edit
@@ -25,6 +25,23 @@ module Administrate
 
     # POST /articles or /articles.json
     def create
+      @category = Category.new(category_params)
+
+      respond_to do |format|
+        if @category.save
+          format.html { redirect_to(administrate_category_url(@category), notice: "Categoria criada com sucesso!") }
+          format.json { render(:show, status: :created, location: @category) }
+        else
+          format.html { render(:new, status: :unprocessable_entity) }
+          format.json { render(json: @category.errors, status: :unprocessable_entity) }
+        end
+      end
+    end
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
     end
   end
 end
