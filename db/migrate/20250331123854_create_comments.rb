15a3f8c9d3a9eff762da29247e5fd91eba342461
Ihago estudos <ihago.oliveira@gmail.com>
2025-03-31 14:24:49 -0300

    feat: habilitando comentarios anonimos

--- db/migrate/20250331123854_create_comments.rb
+++ db/migrate/20250331123854_create_comments.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
-    create_table :comments, id: :uuid do |t|
-      t.references :user, null: false, foreign_key: true, type: :uuid
-      t.references :article, null: false, foreign_key: true, type: :uuid
-      t.text :body
+    create_table(:comments, id: :uuid) do |t|
+      t.references(:user, null: true, foreign_key: true, type: :uuid)
+      t.references(:article, null: false, foreign_key: true, type: :uuid)
+      t.text(:body)
 
       t.timestamps
     end
