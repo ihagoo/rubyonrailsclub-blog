7d997cd93ae9a1323e8b841d047ae1d32600e0ae
Ihago estudos <ihago.oliveira@gmail.com>
2025-04-02 10:25:20 -0300

    fix: validações com rails

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,16 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
     @article = Article.friendly.find(params[:article_id])
-    @article.comments.create!(comments_params)
+    comment = @article.comments.new(comments_params)
 
-    redirect_to(article_path(@article))
+    if comment.save
+      redirect_to(article_path(@article), notice: "Comentário criado com sucesso!")
+    else
+      redirect_to(
+        article_path(@article),
+        alert: "Erro ao criar comentário! - #{comment.errors.full_messages.join(",")}",
+      )
+    end
   end
 
   private
