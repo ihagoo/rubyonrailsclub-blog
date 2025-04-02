11ab64174e10cf0636230b65db2ad32f798fc83b
Ihago estudos <ihago.oliveira@gmail.com>
2025-04-02 09:57:10 -0300

    fix: redirect login e logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_footer_data
+  before_action :store_user_location!, if: :storable_location?
 
   def set_footer_data
     return if params[:controller].split("/").first == "administrate"
@@ -9,5 +10,27 @@ class ApplicationController < ActionController::Base
     @footer_categories_1 = Category.all.order(created_at: :asc).limit(4)
     @footer_categories_2 = Category.all.order(created_at: :desc).limit(4)
     @footer_articles = Article.all.sample(4)
+  end
+
+  private
+
+  def storable_location?
+    request.get? &&
+      is_navigational_format? &&
+      !devise_controller? &&
+      !request.xhr? &&
+      !turbo_frame_request?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referer if resource_or_scope == :user
   end
 end
